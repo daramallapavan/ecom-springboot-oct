19ced3eabdbd6b75a6d30b8cca04d888719789c2
daramallapavan <daramallapavan123@gmail.com>
2024-10-14 09:25:27 +0530

    get api changes

--- src/main/java/com/example/Ecom_SpringBoot/EcomSpringBootApplication.java
+++ src/main/java/com/example/Ecom_SpringBoot/EcomSpringBootApplication.java
@@ -2,12 +2,14 @@ package com.example.Ecom_SpringBoot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class EcomSpringBootApplication {
 
+	@GetMapping("/get")
 	public String get(){
 		return "Ecommerce SpringBoot.....";
 	}
